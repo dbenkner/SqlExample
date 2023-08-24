b473fa8a2ea52f28b3772504d65743ac14f1dd92
David Benkner <d.benkner@gmail.com>
2023-08-24 10:45:32 -0400

    Made Customers class and collection

--- SqlExample/Program.cs
+++ SqlExample/Program.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.Data.SqlClient;
+using SqlExample;
 
-var connStr = "server=localhost\\sqlexpress;" +
-    "database=SalesDb;" +
-    "trusted_connection=true;" +
-    "trustServerCertificate=true;";
+var connStr = "server=localhost\\sqlexpress;" + // server part hostaddressorIP\instance can have more than one instance per machine always have semi colon
+    "database=SalesDb;" + // Database you want to connect to.
+    "trusted_connection=true;" + // uid = userID; pwd=myPasswrod; - normally would use the userID and pw unless sql is being run locally on the machine 
+    "trustServerCertificate=true;"; // Security Certificate 
 var conn = new SqlConnection(connStr);
 
 conn.Open();
@@ -16,55 +17,24 @@ Console.WriteLine("Success!");
 
 // put our sql code here
 
-var sql = "SELECT * FROM customers WHERE ID between 10 and 19;";
-var cmd = new SqlCommand(sql, conn);
-var reader = cmd.ExecuteReader();
+var sql = "SELECT * FROM customers Order By Name"; // sql satement
+var cmd = new SqlCommand(sql, conn); // communicates with sql server to run statement/command
+var reader = cmd.ExecuteReader(); // only done if it is a select statment returns an instance of sql reader
+var customers = new List<Customer>();
 while(reader.Read())
 {
-    var id = Convert.ToInt32(reader["ID"]);
-    var name = Convert.ToString(reader["Name"]);
-    var city = Convert.ToString(reader["City"]);
-    var state = Convert.ToString(reader["state"]);
-    Console.WriteLine($"ID={id}, name={name}, city={city}, State={state}");
-};
+    var cust = new Customer();
+    cust.Id = Convert.ToInt32(reader["ID"]); // data in columns
+    cust.Name = Convert.ToString(reader["Name"]); // nchar
+    cust.City = Convert.ToString(reader["City"]); // 
+    cust.State = Convert.ToString(reader["state"]); // 
+    cust.Sales = Convert.ToDecimal(reader["sales"]);
+    cust.Active = Convert.ToBoolean(reader["Active"]);
+    customers.Add(cust); //
+}
+reader.Close();
+
+// var totalSales  = cmd.ExecuteScalar();   decimal? Sales = reader["Sales"].Equals(DBNull.Value) ? null : Convert.ToDecimal(reader["Sales"]); //? allows decimal to be null
 conn.Close();
 
-conn.Open();
-
-if (conn.State != System.Data.ConnectionState.Open)
-{
-    throw new Exception("Connection didn't open");
-}
-Console.WriteLine("Success!");
-
-sql = "SELECT product, count(product) As TotalSold, count(product) * price as TotalSales FROM Orderlines group by product, Price;";
-cmd = new SqlCommand(sql, conn);
-reader = cmd.ExecuteReader();
-
-while (reader.Read())
-{
-
-    var product = Convert.ToString(reader["Product"]);
-    var count = Convert.ToInt32(reader["TotalSold"]);
-    var totalsold = Convert.ToDecimal(reader["TotalSales"]);
-    Console.WriteLine($"Product={product}, TotalSold = {count}, TotalSales = {totalsold}");
-}
-conn.Close();
-
-conn.Open();
-if (conn.State != System.Data.ConnectionState.Open)
-{
-    throw new Exception("Connection didn't open");
-}
-Console.WriteLine("Scucess!");
-sql = "SELECT c.name, o.description, o.date FROM customers c  JOIN orders o on c.ID = o.CustomerId;";
-cmd = new SqlCommand(sql, conn);
-reader = cmd.ExecuteReader();
-while (reader.Read())
-{
-    var cname = Convert.ToString(reader["c.name"]);
-    var descrip = Convert.ToString(reader["o.description"]);
-    var date = Convert.ToDateTime(reader["o.date"]);
-    Console.WriteLine($"Customer Name = {cname}, Order Description:{descrip}, Date = {date}");
-}
-conn.Close();
+var x = 0;
