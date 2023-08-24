2753168753ba0f7e6020c0c7367d3df978320c53
David Benkner <d.benkner@gmail.com>
2023-08-24 07:18:09 -0400

    Played around some more

--- SqlExample/Program.cs
+++ SqlExample/Program.cs
@@ -24,8 +24,47 @@ while(reader.Read())
     var id = Convert.ToInt32(reader["ID"]);
     var name = Convert.ToString(reader["Name"]);
     var city = Convert.ToString(reader["City"]);
-    Console.WriteLine($"ID={id}, name={name}, city={city}");
+    var state = Convert.ToString(reader["state"]);
+    Console.WriteLine($"ID={id}, name={name}, city={city}, State={state}");
 };
+conn.Close();
 
+conn.Open();
 
+if (conn.State != System.Data.ConnectionState.Open)
+{
+    throw new Exception("Connection didn't open");
+}
+Console.WriteLine("Success!");
+
+sql = "SELECT product, count(product) As TotalSold, count(product) * price as TotalSales FROM Orderlines group by product, Price;";
+cmd = new SqlCommand(sql, conn);
+reader = cmd.ExecuteReader();
+
+while (reader.Read())
+{
+
+    var product = Convert.ToString(reader["Product"]);
+    var count = Convert.ToInt32(reader["TotalSold"]);
+    var totalsold = Convert.ToDecimal(reader["TotalSales"]);
+    Console.WriteLine($"Product={product}, TotalSold = {count}, TotalSales = {totalsold}");
+}
+conn.Close();
+
+conn.Open();
+if (conn.State != System.Data.ConnectionState.Open)
+{
+    throw new Exception("Connection didn't open");
+}
+Console.WriteLine("Scucess!");
+sql = "SELECT c.name, o.description, o.date FROM customers c  JOIN orders o on c.ID = o.CustomerId;";
+cmd = new SqlCommand(sql, conn);
+reader = cmd.ExecuteReader();
+while (reader.Read())
+{
+    var cname = Convert.ToString(reader["c.name"]);
+    var descrip = Convert.ToString(reader["o.description"]);
+    var date = Convert.ToDateTime(reader["o.date"]);
+    Console.WriteLine($"Customer Name = {cname}, Order Description:{descrip}, Date = {date}");
+}
 conn.Close();
